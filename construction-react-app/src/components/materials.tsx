a1dbe8f6fa23915cae14f68f2e901688ca2ceb2a
redricasa <fredeb26@gmail.com>
2023-11-09 13:17:23 -0800

    added the address entry section

--- construction-react-app/src/components/materials.tsx
+++ construction-react-app/src/components/materials.tsx
@@ -4,7 +4,6 @@ const Materials = () => {
     return (
         <>
         <form>
-
             {/* bulk purchase selection */}
             <div className="form-group">
             <label htmlFor="exampleFormControlSelect2">Bulk Purchase</label>
@@ -21,11 +20,72 @@ const Materials = () => {
                 <option>New</option>
             </select>
             </div>
-        </form>
 
-        <button type="button" className="btn btn-info">
+            {/* street address entry start*/}
+            <div className="container mt-12">
+            <div className="mb-8">
+                <label htmlFor="street" className="form-label">
+                Street
+                </label>
+                <input
+                type="text"
+                className="form-control"
+                id="street"
+                name="street"
+                // value={address.street}
+                // onChange={handleChange}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="city" className="form-label">
+                City
+                </label>
+                <input
+                type="text"
+                className="form-control"
+                id="city"
+                name="city"
+                // value={address.city}
+                // onChange={handleChange}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="state" className="form-label">
+                State
+                </label>
+                <input
+                type="text"
+                className="form-control"
+                id="state"
+                name="state"
+                // value={address.state}
+                // onChange={handleChange}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="zipcode" className="form-label">
+                Zipcode
+                </label>
+                <input
+                type="text"
+                className="form-control"
+                id="zipcode"
+                name="zipcode"
+                // value={address.zipcode}
+                // onChange={handleChange}
+                />
+            </div>
+            
+            {/* street address entry end */}
+            {/* submit button */}
+            <button type="button" className="btn btn-info">
             Submit
-        </button>
+            </button>
+            </div>
+        </form>
         </>
     );
 };
