af300a5d24fb339a3ac3885a7de1797d6824f37f
redricasa <fredeb26@gmail.com>
2023-11-16 13:48:36 -0800

    added controller and routes directories with files

---
+++ construction-react-app/server/controllers/userController.js
@@ -0,0 +1,11 @@
+// @desc Auth user / set token
+// route POST /api/users/auth
+// @access public
+const authUser = (req, res) => {
+    res.status(200).json({message: 'testing'})
+}
+
+export {
+    authUser
+    
+}
