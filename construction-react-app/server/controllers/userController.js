fe3be0a98c07cd048ea8fcef3f75965e48f245e4
redricasa <fredeb26@gmail.com>
2023-11-18 23:38:49 -0800

    added logic to check for email and pass matching db for login

--- construction-react-app/server/controllers/userController.js
+++ construction-react-app/server/controllers/userController.js
@@ -1,10 +1,25 @@
 import asyncHandler from 'express-async-handler'; 
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
+
 // @desc Auth user / set token
 // route POST /api/users/auth
 // @access public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'auth user' });
+    const { email, password } = req.body;
+    const user = await User.findOne({email})
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email:user.email
+        }); 
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 // @desc register new user
 // route POST /api/users
@@ -25,6 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
     });
 
     if (user) {
+        generateToken(res, user._id)
         res.status(201).json({
             _id: user._id,
             name: user.name,
--- construction-react-app/server/models/userModel.js
+++ construction-react-app/server/models/userModel.js
@@ -26,7 +26,11 @@ userSchema.pre('save', async function (next) {
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt); 
-})
+});
+// returns true if passwords match
+userSchema.methods.matchPasswords = async function(enteredPass) {
+    return await bcrypt.compare(enteredPass, this.password);
+}
 
 const User = mongoose.model('User', userSchema);
 
