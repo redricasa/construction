cbe50f9cb4f69bbed608e555c1e5933b44de9cf3
redricasa <fredeb26@gmail.com>
2023-11-20 01:04:19 -0800

    added concurrently to run backend and frontend concurrently + fixed previous bug by changing userID to userId in generateToken.js

--- construction-react-app/server/middleware/authMiddleware.js
+++ construction-react-app/server/middleware/authMiddleware.js
@@ -9,14 +9,11 @@ const protect = asyncHandler(async (req, res, next) => {
 
     if (token) {
         try {
-            // console.log("Before Token Verification:", token);
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            console.log("After Token Verification:", decoded);// Log the decoded object
             // set req.user to user logged in once token is decoded
             req.user = await User.findById(decoded.userId).select('-password');// -password makes sure that the hashed password isn't returned
             next();
         } catch (error) {
-            // console.error("Error during token verification:", error);
             res.status(401)
             throw new Error('Not authorized, token exists but is invalid')
         }
