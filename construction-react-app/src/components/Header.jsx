d9ad484539d9b1ed02cee0819cf3802a8e19c7b4
redricasa <fredeb26@gmail.com>
2023-11-23 23:17:10 -0800

    adding profile and logout in navbar

--- construction-react-app/src/components/Header.jsx
+++ construction-react-app/src/components/Header.jsx
@@ -1,51 +1,76 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap';
-
+import { useSelector, useDispatch } from 'react-redux';
 
 
 const Header = () => {
+    const { userInfo } = useSelector((state) => state.auth)
+
+
     return (
         <header>
         <nav className="navbar sticky-top navbar-light bg-secondary">
             <div className="container-fluid ">
                 <Container>
-                <LinkContainer to='/'>
-                    <Navbar.Brand className="navbar-brand" >Construction Inventory Management</Navbar.Brand>
-                </LinkContainer>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand className="navbar-brand" >Construction Inventory Management</Navbar.Brand>
+                    </LinkContainer>
 
-                
-                <LinkContainer to='/tools'>
-                    <Nav.Link >
-                        <button className="btn btn-outline-light me-2" type="button">
-                            Tools
-                        </button>
-                    </Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/materials'>
-                    <Nav.Link >
-                        <button className="btn btn-outline-light" type="button">
-                            Materials
-                        </button>
-                    </Nav.Link>
-                </LinkContainer>
+                    <Nav className='ms-auto' >
+                        { userInfo ? (
+                            <>
+                                <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item >
+                                        Profile
+                                    </NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item >
+                                    Log out
+                                </NavDropdown.Item>
+                                </NavDropdown>
+                            </>
+                        ) : (
+                            <>
+                            <LinkContainer to='/login'>
+                                <Nav.Link >
+                                <button className="btn btn-outline-warning me-2" type="button">
+                                    Log In
+                                </button>
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/register'>
+                                <Nav.Link >
+                                <button className="btn btn-sm btn-outline-warning" type="button">
+                                    Register
+                                </button>
+                                </Nav.Link>
+                            </LinkContainer>
+                            </>
+                        )}
+                    <LinkContainer to='/tools'>
+                        <Nav.Link >
+                            <button className="btn btn-outline-light me-2" type="button">
+                                Tools
+                            </button>
+                        </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/materials'>
+                        <Nav.Link >
+                            <button className="btn btn-outline-light" type="button">
+                                Materials
+                            </button>
+                        </Nav.Link>
+                    </LinkContainer>
         
-                {/* sign in/up start ---*/}
-                <LinkContainer to='/login'>
-                    <Nav.Link >
-                        <button className="btn btn-outline-warning me-2" type="button">
-                            Log In
-                        </button>
-                    </Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/register'>
-                    <Nav.Link >
-                        <button className="btn btn-sm btn-outline-warning" type="button">
-                            Register
-                        </button>
-                    </Nav.Link>
-                </LinkContainer>
+                
+
+                    {/* sign in/up start ---*/}
+                    
+                    
+                    </Nav>
+        
                 </Container>
-            {/* sign in/up end--- */}
             </div>
         </nav>
         
